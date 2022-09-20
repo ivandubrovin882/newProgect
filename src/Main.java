b9393e51d41e8d4e1d373fa4ace9da5ce772d72e
ivand <ivan241985@mail.ru>
2022-09-21 01:19:38 +0300

    Сдано

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет!");
         double dog = 8;
         System.out.println(dog);
         var cat = 3.6;
